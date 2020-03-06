29290ce56d89cdd935a35d3ea4b6dffc0513943c
Benjamin Denom <ben.denom@gmail.com>
2020-03-06 15:06:09 +0100

    Fix Symbol.iterator not returning a new instance

--- src/LazySeq.ts
+++ src/LazySeq.ts
@@ -1,16 +1,16 @@
 class LazySeq<T> implements Iterable<T> {
-    constructor(private iterator: Iterator<T>) {}
+    constructor(private iterator: () => Iterator<T>) {}
     
-    [Symbol.iterator]() {
-        return this.iterator
+    [Symbol.iterator](): Iterator<T> {
+        return this.iterator()
     }
 
     static of<T>(...arr: Array<T>){
-        return new LazySeq<T>(arr[Symbol.iterator]())
+        return new LazySeq<T>(() => arr[Symbol.iterator]())
     }
 
     static from<T>(iterable: Iterable<T>): LazySeq<T> {
-        return new LazySeq<T>(iterable[Symbol.iterator]())
+        return new LazySeq<T>(() => iterable[Symbol.iterator]())
     }
 
     toArray(): Array<T> {
@@ -18,13 +18,8 @@ class LazySeq<T> implements Iterable<T> {
     }
 }
 
-const xs = LazySeq.from(new Set([1, 1, 1, 2, 3]));
-const ys = LazySeq.of(7, 8, 9);
-const zs = ys.toArray(); // todo: may not shared the same iterable
-
-for(let z of zs) {
-    console.log(z);
-}
+const xs = LazySeq.from(new Set([1, 1, 1, 2, 3, 4]));
+const ys = xs.toArray();
 
 for(let y of ys) {
     console.log(y);
