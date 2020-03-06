95cab45990a5a6e1fddb7e090b90292525035d94
Benjamin Denom <benjamin@dotlol.fr>
2020-03-06 08:29:40 +0100

    Add LazySeq.toArray (wip)

--- src/LazySeq.ts
+++ src/LazySeq.ts
@@ -9,16 +9,25 @@ class LazySeq<T> implements Iterable<T> {
         return new LazySeq<T>(arr[Symbol.iterator]())
     }
 
-    static from<T>(iterable: Iterable<T> | ArrayLike<T>): LazySeq<T> {
+    static from<T>(iterable: Iterable<T>): LazySeq<T> {
         return new LazySeq<T>(iterable[Symbol.iterator]())
+    }
+
+    toArray(): Array<T> {
+        return Array.from(this)
     }
 }
 
-const xs = LazySeq.from(new Set([1, 1, 1, 2, 3]))
-const ys = LazySeq.of(7, 8, 9)
+const xs = LazySeq.from(new Set([1, 1, 1, 2, 3]));
+const ys = LazySeq.of(7, 8, 9);
+const zs = ys.toArray(); // todo: may not shared the same iterable
 
-for(let x of xs) {
-    console.log(x);
+for(let z of zs) {
+    console.log(z);
+}
+
+for(let y of ys) {
+    console.log(y);
 }
 
 for(let y of ys) {
