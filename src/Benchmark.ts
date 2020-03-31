079e9ff163e3d0eea1e0d50efcb2815fd4d0c29e
Benjamin Denom <ben.denom@gmail.com>
2020-03-31 22:56:49 +0200

    Trying another (yes again) new but classic approach to implement Option<T>

--- src/Benchmark.ts
+++ src/Benchmark.ts
@@ -1,21 +1,30 @@
-import { Some } from './Option';
-import { Some_WIP as WIP } from './Option.wip';
+import { Some as OldSome } from './Option';
+import { Some } from './Option.wip';
 
 const Benchmark = require('benchmark');
 const suite = new Benchmark.Suite;
 
-console.log(WIP(1337));
-
 let i = 0;
+let minusOne = (x: number) => x - 1;
+let plusOne = (x: number) => x + 1;
+
+const s1 = OldSome(100);
+const s2 = Some(100);
+
+console.log(Some(30));
 
 suite
-.add('Some(10)', function() {
-  const x = Some(i++);
-  // const y = x.value;
+.add('Some Creation', function() {
+  const x = OldSome(i++);
 })
-.add('WIP(10)', function() {
-  const x = WIP(i++);
-  // const y = x.value;
+.add('WIP Creation', function() {
+  const x = Some(i--);
+})
+.add('Some Mapping', function() {
+  const y = s1.map(plusOne).map(minusOne);
+})
+.add('WIP Mapping', function() {
+  const y = s2.map(minusOne).map(plusOne);
 })
 .on('cycle', function(event) {
   console.log(String(event.target));
--- src/Option.wip.ts
+++ src/Option.wip.ts
@@ -1,34 +1,13 @@
-enum OptionType {
-  Some = '__some',
-  None = '__none',
+class Some<T> {
+  constructor(private value: T) {}
+
+  map<U>(f: (x: T) => U): Some<U> {
+    return new Some(f(this.value));
+  }
 }
 
-export interface Option_WIP<T> {
-  isEmpty: boolean,
-  map<U>(f: (x: T) => U): Option_WIP<U>;
-  flatMap<U>(f: (x: T) => Option_WIP<U>): Option_WIP<U>;
+function SomeConstructor<T>(value: T) {
+  return new Some(value);
 }
 
-export interface Some_WIP<T> extends Option_WIP<T> {
-  type: OptionType.Some;
-  get(): T;
-}
-
-export interface None_WIP extends Option_WIP<never> {
-  type: OptionType.None;
-}
-
-export const None_WIP = (): None_WIP => ({
-  type: OptionType.None,
-  isEmpty: true,
-  map: <U>(f: (x: never) => U): Option_WIP<U> => None_WIP(),
-  flatMap: <U>(f: (x: never) => Option_WIP<U>): Option_WIP<U> => None_WIP()
-});
-
-export const Some_WIP = <T>(value: T): Some_WIP<T> => ({
-  type: OptionType.Some,
-  isEmpty: false,
-  get: () => value,
-  flatMap: <U>(f: (x: T) => Option_WIP<U>): Option_WIP<U> => f(value),
-  map: <U>(f: (x: T) => U): Option_WIP<U> => Some_WIP(f(value))
-});
+export { SomeConstructor as Some };
